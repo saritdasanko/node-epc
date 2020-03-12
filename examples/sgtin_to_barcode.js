15ce8845965cb202e66747343a6ff3c16b3a8611
Sarit Das <sarit.das@anko.com>
2020-03-12 11:25:24 -0700

    changes

---
+++ examples/sgtin_to_barcode.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var epc = require('../');
+//var epc = require('node-epc');
+
+let example = "30340129FC54C544A817C812"
+
+// Parse using a specific encoding
+epc.getParser('SGTIN')
+	.then(function(sgtin) {
+		sgtin.parse(example)
+			.then(function(parsed) {
+				// console.log('Encoding = ' + parsed.getName());
+				// console.log('Company Prefix = ' + parsed.parts.CompanyPrefix);
+				// console.log('Item Reference = ' + parsed.parts.ItemReference);
+				// console.log('Serial Number = ' + parsed.parts.SerialNumber);
+
+                let barcodeWOCheckBit = parsed.parts.CompanyPrefix + parsed.parts.ItemReference
+                let barcodeWCheckBit = [0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+                for(let i = 1; i < barcodeWOCheckBit.length; i++) {
+                    if((i+1)%2 == 0) {
+                        let val = parseInt(barcodeWOCheckBit[i]) * 3;
+                        barcodeWCheckBit[i] = barcodeWOCheckBit[i];
+                        barcodeWCheckBit[barcodeWOCheckBit.length] += val;
+                    } else {
+                        let val = parseInt(barcodeWOCheckBit[i]) * 1;
+                        barcodeWCheckBit[i] = barcodeWOCheckBit[i];
+                        barcodeWCheckBit[barcodeWOCheckBit.length] += val;
+                    }
+                }
+
+                barcodeWCheckBit[barcodeWOCheckBit.length] = Math.abs((Math.round(barcodeWCheckBit[barcodeWOCheckBit.length]/10)*10) -  barcodeWCheckBit[barcodeWOCheckBit.length]);
+                console.debug("Barcode: ", barcodeWCheckBit.join(''));
+			})
+			.fail(function(err) {
+				console.error(err);
+			});
+	});

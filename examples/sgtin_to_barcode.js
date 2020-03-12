4052d525ac943f1cfffe58e774e3883738f8e50b
Sarit Das <sarit.das@anko.com>
2020-03-12 15:33:38 -0700

    working with promises

--- examples/sgtin_to_barcode.js
+++ examples/sgtin_to_barcode.js
@@ -3,16 +3,32 @@
 var epc = require('../');
 
 //var epc = require('node-epc');
-let example = "30363A22CC00AB437E11D630"
+let epctag = "30340129FC54C544A817C812";
 
 
-epc.getParser('SGTIN')
-	.then(function(sgtin) {
-		sgtin.parse(example)
-			.then(function(parsed) {
-               console.log(parsed.parts.gtin13);
-			})
-			.fail(function(err) {
-				console.error(err);
-			});
-	});
+function parseEPC(epctag) {
+    return new Promise(async (resolve, reject) => {
+        await epc.getParser('SGTIN')
+            .then(async function(sgtin) {
+                await sgtin.parse(epctag)
+                    .then(function(parsed) {
+                        // console.log(parsed.parts.gtin13);
+                        resolve(parsed.parts.gtin13);
+                    })
+                    .fail(function(err) {
+                        console.error(err);
+                        reject(err);
+                    });
+            });
+    });
+}
+
+async function demo() {
+    let val = await parseEPC(epctag);
+    console.log(val);
+}
+
+
+demo();
+
+
